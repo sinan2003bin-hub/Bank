8b0ef26a7752c9fa522555d0afeeed9172c84629
Samia <sinan2003bin@gmail.com>
2026-06-12 19:18:48 +0600

    upgraded dashboard and UI a bit. made buttons workable

--- Bank_system/models.py
+++ Bank_system/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Bank(models.Model):
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100, unique=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    balance = models.FloatField(default=0)
+    total_deposit = models.FloatField(default=0)
+    total_withdraw = models.FloatField(default=0)
+    deposit_count = models.IntegerField(default=0)
+    withdraw_count = models.IntegerField(default=0)
 
-    def __str__(self):
-        return self.username
--- Bank_system/views.py
+++ Bank_system/views.py
@@ -1,11 +1,8 @@
-from urllib import request
-
-from django.http import HttpResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import render, redirect
+from .models import Bank
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import logout
 
 def home(request):
 
@@ -16,20 +13,19 @@ def home(request):
         user = authenticate(
             request,
             username=username,
-            password=password
-        )
+            password=password)
 
         if user is not None:
             login(request, user)
-
             return redirect("dashboard")
-        
-        return render(request, "home.html", 
-                {
-                    "error": "Password or username is incorrect"
-                }
+
+        return render(request,"home.html",
+        {
+            "error":
+            "Password or username is incorrect"
+        }
             )
-    
+
     return render(request, "home.html")
 
 def signup(request):
@@ -45,28 +41,70 @@ def signup(request):
         if password != confirm_password:
 
             return render(request,"signup.html",
-                {
-                    "error":"Passwords do not match"
-                }
-            )
+            {
+                "error":
+                "Passwords do not match"
+            }
+                )
 
-        User.objects.create_user(
+        user = User.objects.create_user(
             username=username,
             password=password,
-            email=email
-        )
+            email=email)
+
+        Bank.objects.create(user=user,balance=0)
 
         return redirect("home")
 
-    return render(request,"signup.html")
+    return render(request, "signup.html")
+
 
 @login_required
 def dashboard(request):
-    return render(request,"dashboard.html")
+
+    bank, created = Bank.objects.get_or_create(
+    user=request.user,
+    defaults={"balance": 0}
+    )
+
+
+    action = None
+
+    if request.method == "POST":
+
+        action = request.POST.get("action")
+
+        if action == "deposit" and "amount" in request.POST:
+
+            amount = float(request.POST["amount"])
+
+            bank.balance += amount
+            bank.total_deposit += amount
+            bank.deposit_count += 1
+
+            bank.save()
+
+        elif action == "withdraw" and "amount" in request.POST:
+
+            amount = float(request.POST["amount"])
+
+            if amount <= bank.balance:
+
+                bank.balance -= amount
+                bank.total_withdraw += amount
+                bank.withdraw_count += 1
+
+                bank.save()
+
+    return render(request,"dashboard.html",
+        {
+            "bank": bank,
+            "action": action
+        }
+            )
+
 
 def logout_page(request):
-
     logout(request)
 
     return redirect("home")
-
